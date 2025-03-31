595d7c985e160d93382ff7b5e278f1db3248c66b
toumimohamedamine <toumi.mohamedamine@esprit.tn>
2025-03-31 05:01:04 +0100

    Dewini

---
+++ backend/projet/src/main/java/com/pi/projet/BachendApplication.java
@@ -0,0 +1,13 @@
+package com.pi.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BachendApplication.class, args);
+	}
+
+}
---
+++ backend/projet/src/test/java/com/pi/projet/BachendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
